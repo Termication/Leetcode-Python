362f0dd13ff0fa091a7809312724043ed2de33d9
Innocent Karabo Mohlala <124525523+Termication@users.noreply.github.com>
2025-11-29 22:25:38 +0200

    Time: 0 ms (100.00%) | Memory: 12.7 MB (47.47%) - LeetSync

---
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        total = sum(nums)
+        remainder = total % k
+        return remainder
+        
